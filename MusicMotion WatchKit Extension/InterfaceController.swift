0efb906dc0cd440ef2ff422f308428b6f193362b
rrlara <rrlara@gmail.com>
2015-08-18 21:17:45 -0700

    Able to display current activity on the apple watch

--- MusicMotion WatchKit Extension/InterfaceController.swift
+++ MusicMotion WatchKit Extension/InterfaceController.swift
@@ -9,6 +9,8 @@
 import WatchKit
 import Foundation
 
+import CoreMotion
+
 
 class InterfaceController: WKInterfaceController {
 
@@ -16,15 +18,75 @@ class InterfaceController: WKInterfaceController {
     
     @IBOutlet weak var currentActivityImage: WKInterfaceImage!
     
+    
+    
+    let dataProcessingQueue = NSOperationQueue()
+    let activityManager = CMMotionActivityManager()
+    let lengthFormatter = NSLengthFormatter()
+    
+    let walkImage = UIImage(named: "walk")
+    let runnerImage = UIImage(named: "run")
+    let cycleImage = UIImage(named: "cycle")
+    let automotiveImage = UIImage(named: "automotive")
+    let stationaryImage = UIImage(named: "stationary")
+    
+    var altChange: Double = 0
+    
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
         // Configure interface objects here.
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        
+        // Prepare activity updates
+        activityManager.startActivityUpdatesToQueue(dataProcessingQueue) {
+            data in
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                
+                if data.confidence.rawValue > 0{
+                    if data.running {
+
+//                        [self.currentActivityImage .setImage(UIImage(named: "run"))]
+                        self.currentActivityImage.setImage(self.runnerImage)
+                        self.currentActivityLabel.setText("running")
+                    } else if data.cycling {
+//                        [self.currentActivityImage .setImage(UIImage(named: "cycle"))]
+                        self.currentActivityImage.setImage(self.cycleImage)
+                        self.currentActivityLabel.setText("cycling")
+                    } else if data.walking {
+                        self.currentActivityImage.setImage(self.walkImage)
+                        self.currentActivityLabel.setText("walking")
+                    }
+                    else if data.stationary {
+//                        [self.currentActivityImage .setImage(UIImage(named: "stationary"))]
+                        self.currentActivityImage.setImage(self.stationaryImage)
+                        self.currentActivityLabel.setText("stationary")
+                    }
+                    else if data.automotive {
+//                        [self.currentActivityImage .setImage(UIImage(named: "automotive"))]
+                        self.currentActivityImage.setImage(self.automotiveImage)
+                        self.currentActivityLabel.setText("automotive")
+                    }
+                    else {
+//                        [self.currentActivityImage .setImage(UIImage(named: "nil"))]
+                        self.currentActivityLabel.setText("no idea")
+                    }
+                }
+                        println(data)
+                
+            }
+            
+            
+        }
+
+        
+        
     }
 
     override func didDeactivate() {
